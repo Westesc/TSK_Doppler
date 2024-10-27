411348a0cb520fa7fa9bfd5137a773feeaf0cb32
ames123 <126474987+ames123@users.noreply.github.com>
2024-10-27 17:07:15 +0100

    add check value

--- Assets/Scripts/Doppler.cs
+++ Assets/Scripts/Doppler.cs
@@ -23,6 +23,257 @@ public class Doppler : MonoBehaviour
         fs
     }
     public Car car;
+    private bool onButton = false;
+
+    public bool Check()
+    {
+        switch (chooseVersion)
+        {
+            case Version.vs:
+                if (vo != 0 && fs != 0 && fo != 0)
+                {
+                    calculateOnlyVs();
+                    if (vs < 140 && vs > 1)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                break;
+            case Version.vo:
+                if (vs != 0 && fs != 0 && fo != 0)
+                {
+                    calculateOnlyVo();
+                    if (vo < 140 && vo > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                break;
+            case Version.fo:
+                if (vo != 0 && fs != 0 && vs != 0)
+                {
+                    calculateOnlyFo();
+                    if (fo < 2000 && fo > 20)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                break;
+            case Version.fs:
+                if (vo != 0 && fo != 0 && vs != 0)
+                {
+                    calculateOnlyFs();
+                    if (fs < 2000 && fs > 20)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                break;
+        }
+        return true;
+    }
+
+    public float getMinValueFs()
+    {
+        switch (chooseVersion)
+        {
+            case Version.vs:
+                calculateFsWithVariable(vo, 1,fo);
+                break;
+
+            case Version.vo:
+                calculateFsWithVariable(1,vs,fo);
+                break;
+
+            case Version.fo:
+                calculateFsWithVariable(vo,vs,20);
+                break;
+        }
+        return fs_;
+    }
+    public float getMaxValueFs()
+    {
+        switch (chooseVersion)
+        {
+            case Version.vs:
+                calculateFsWithVariable(vo, 140, fo);
+                break;
+
+            case Version.vo:
+                calculateFsWithVariable(140, vs, fo);
+                break;
+
+            case Version.fo:
+                calculateFsWithVariable(vo, vs, 2000);
+                break;
+        }
+        return fs_;
+    }
+    public float getMinValueFo()
+    {
+        switch (chooseVersion)
+        {
+            case Version.vs:
+                calculateFoWithVariable(vo, 1, fs);
+                break;
+
+            case Version.vo:
+                calculateFoWithVariable(1, vs, fs);
+                break;
+
+            case Version.fs:
+                calculateFoWithVariable(vo, vs, 20);
+                break;
+        }
+        return fo_;
+    }
+    public float getMaxValueFo()
+    {
+        switch (chooseVersion)
+        {
+            case Version.vs:
+                calculateFoWithVariable(vo, 140, fs);
+                break;
+
+            case Version.vo:
+                calculateFoWithVariable(140, vs, fs);
+                break;
+
+            case Version.fs:
+                calculateFoWithVariable(vo, vs, 2000);
+                break;
+        }
+        return fo_;
+    }
+
+    public float getMinValueVs()
+    {
+        switch (chooseVersion)
+        {
+            case Version.fo:
+                calculateVsWithVariable(vo, fs, 20);
+                break;
+
+            case Version.vo:
+                calculateVsWithVariable(1, fs, fo);
+                break;
+
+            case Version.fs:
+                calculateVsWithVariable(vo, 20, fo);
+                break;
+        }
+        return vs_;
+    }
+    public float getMaxValueVs()
+    {
+        switch (chooseVersion)
+        {
+            case Version.fo:
+                calculateVsWithVariable(vo, fs, 2000);
+                break;
+
+            case Version.vo:
+                calculateVsWithVariable(140, fs, fo);
+                break;
+
+            case Version.fs:
+                calculateVsWithVariable(vo, 2000, fo);
+                break;
+        }
+        return vs_;
+    }
+
+    public float getMinValueVo()
+    {
+        switch (chooseVersion)
+        {
+            case Version.fo:
+                calculateVoWithVariable(fs, vs, 20);
+                break;
+
+            case Version.vs:
+                calculateVoWithVariable(fs, 1, fo);
+                break;
+
+            case Version.fs:
+                calculateVoWithVariable(20, vs, fo);
+                break;
+        }
+        return vo_;
+    }
+    public float getMaxValueVo()
+    {
+        switch (chooseVersion)
+        {
+            case Version.fo:
+                calculateVoWithVariable(fs, vs, 2000);
+                break;
+
+            case Version.vs:
+                calculateVoWithVariable(fs, 140, fo);
+                break;
+
+            case Version.fs:
+                calculateVoWithVariable(2000, vs, fo);
+                break;
+        }
+        return vo_;
+    }
+
+
+    float fs_, fo_, vo_, vs_;
+    public void calculateFsWithVariable(float vo, float vs, float fo)
+    {
+        if (s >= 0)
+        {
+            fs_ = fo * (v + vs) / (v + vo);
+        }
+        else
+        {
+            fs_ = fo * (v - vs) / (v - vo);
+        }
+    }
+    public void calculateFoWithVariable(float vo, float vs, float fs)
+    {
+        if (s >= 0)
+        {
+            fo_ = (v + vo) / (v + vs) * fs;
+        }
+        else
+        {
+            fo_ = (v - vo) / (v - vs) * fs;
+        }
+    }
+    public void calculateVsWithVariable(float vo, float fs, float fo)
+    {
+        if (s >= 0)
+        {
+            vs_ = fs * (v + vo) / fo - v;
+        }
+        else
+        {
+            vs_ = v - fs * (v - vo) / fo;
+        }
+    }
+    public void calculateVoWithVariable(float fs, float vs, float fo)
+    {
+        if (s >= 0)
+        {
+            vo_ = fo * (v + vs) / fs - v;
+        }
+        else
+        {
+            vo_ = v - fo * (v - vs)/fs;
+        }
+    }
+    public  void setButton()
+    {
+        onButton = !onButton;
+    }
 
     public void SetVo(float value)
     {
@@ -47,39 +298,79 @@ public class Doppler : MonoBehaviour
 
     void Update()
     {
-        switch (chooseVersion)
+        if (onButton)
         {
-            case Version.vs:
-                calculateVs();
-                break;
+            switch (chooseVersion)
+            {
+                case Version.vs:
+                    calculateVs();
+                    break;
 
-            case Version.vo:
-                calculateVo();
-                break;
+                case Version.vo:
+                    calculateVo();
+                    break;
 
-            case Version.fo:
-                calculateFo();
-                break;
+                case Version.fo:
+                    calculateFo();
+                    break;
 
-            case Version.fs:
-                calculateFs();
-                break;
+                case Version.fs:
+                    calculateFs();
+                    break;
+            }
         }
     }
 
+    private void calculateOnlyVs()
+    {
+        if (s >= 0)
+        {
+            vs = fs * (v + vo) / fo - v;
+        }
+        else
+        {
+            vs = v - fs * (v - vo) / fo;
+        }
+    }
+    private void calculateOnlyVo()
+    {
+        if (s >= 0)
+        {
+            vo = fo * (v + vs) / fs - v;
+        }
+        else
+        {
+            vo = v - fo * (v - vs) / fs;
+        }
+    }
+    private void calculateOnlyFo()
+    {
+        if (s >= 0)
+        {
+            fo = (v + vo) / (v + vs) * fs;
+        }
+        else
+        {
+            fo = (v - vo) / (v - vs) * fs;
+        }
+    }
+    private void calculateOnlyFs()
+    {
+        if (s >= 0)
+        {
+            fs = fo * (v + vs) / (v + vo);
+        }
+        else
+        {
+            fs = fo * (v - vs) / (v - vo);
+        }
+    }
     private void calculateVs()
     {
         float deltaTime = Time.deltaTime;
         if (vo != 0 && fs != 0 && fo != 0)
         {
-            if (s >= 0)
-            {
-                vs = fs * (v + vo) / fo - v;
-            }
-            else
-            {
-                vs = v - fs * (v - vo) / fo;
-            }
+            calculateOnlyVs();
             s += deltaTime * (vs - vo);
             tmp_s.text = tmp_s.name + " - " + Mathf.Abs(Mathf.Round(s)) + " m";
             result.text = result.name + " - " + Mathf.Round(vs) + "m/s";
@@ -94,14 +385,7 @@ public class Doppler : MonoBehaviour
         float deltaTime = Time.deltaTime;
         if (vs != 0 && fs != 0 && fo != 0)
         {
-            if (s >= 0)
-            {
-                vo = fo * (v + vs) / fs - v;
-            }
-            else
-            {
-                vo = fo * (v - vs) / fs + v;
-            }
+            calculateOnlyVo();
             s += deltaTime * (vs - vo);
             tmp_s.text = tmp_s.name + " - " + Mathf.Abs(Mathf.Round(s)) + " m";
             result.text = result.name + " - " + Mathf.Round(vo) + "m/s";
@@ -116,14 +400,7 @@ public class Doppler : MonoBehaviour
         float deltaTime = Time.deltaTime;
         if (vs != 0 && vo != 0 && fo != 0)
         {
-            if (s >= 0)
-            {
-                fs = fo * (v + vs) / (v+vo);
-            }
-            else
-            {
-                fs = fo * (v - vs) / (v - vo);
-            }
+            calculateOnlyFs();
             s += deltaTime * (vs - vo);
             tmp_s.text = tmp_s.name + " - " + Mathf.Abs(Mathf.Round(s)) + " m";
             result.text = result.name + " - " + Mathf.Round(fs) + "Hz";
@@ -138,14 +415,7 @@ public class Doppler : MonoBehaviour
         float deltaTime = Time.deltaTime;
         if (vo != 0 && fs != 0 && vs != 0)
         {
-            if (s >= 0)
-            {
-                fo = (v + vo) / (v + vs) * fs;
-            }
-            else
-            {
-                fo = (v - vo) / (v - vs) * fs;
-            }
+            calculateOnlyFo();
             s += deltaTime * (vs - vo);
             tmp_s.text = tmp_s.name + " - " + Mathf.Abs(Mathf.Round(s)) + " m";
             result.text = result.name + " - " + Mathf.Round(fo) + "Hz";
--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -10,6 +10,12 @@ public class SliderScript : MonoBehaviour
     [SerializeField] private TextMeshProUGUI tmp;
     public string unit;
     public Doppler doppler;
+    private float previousFs = 0;
+    private float previousFo = 0;
+    private float previousVs = 0;
+    private float previousVo = 0;
+    float min = 0;
+    float max = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,19 +23,97 @@ public class SliderScript : MonoBehaviour
             tmp.text = tmp.name + " - " + v.ToString("0.") +" "+ unit;
             if (tmp.name == "vo")
             {
-                doppler.SetVo(v); 
+                doppler.SetVo(v);
+                if (!doppler.Check())
+                {
+                    min = doppler.getMinValueVo();
+                    max = doppler.getMaxValueVo();
+                    if (Mathf.Abs(v - max) > Mathf.Abs(v - min))
+                    {
+                        previousVo = min;
+                    }
+                    else
+                    {
+                        Debug.Log("prze³aczam na max");
+                        previousVo = max;
+                    }
+                    Debug.Log("max i min: " + max + " > " + min);
+                    doppler.SetVo(previousVo);
+                    slider.value = previousVo;
+                }
+                else
+                {
+                    previousVo = v;
+                }
             }
             else if (tmp.name == "fs")
             {
                 doppler.SetFs(v);
+                if (!doppler.Check())
+                {
+                    min = doppler.getMinValueFs();
+                    max = doppler.getMaxValueFs();
+                    if(Mathf.Abs(v-max)> Mathf.Abs(v - min))
+                    {
+                        previousFs = min;
+                    }
+                    else
+                    {
+                        previousFs = max;
+                    }
+                    doppler.SetFs(previousFs);
+                    slider.value = previousFs;
+                }
+                else
+                {
+                    previousFs = v;
+                }
             }
             else if (tmp.name == "fo")
             {
                 doppler.SetFo(v);
+                if (!doppler.Check())
+                {
+                    min = doppler.getMinValueFo();
+                    max = doppler.getMaxValueFo();
+                    if (Mathf.Abs(v - max) > Mathf.Abs(v - min))
+                    {
+                        previousFo = min;
+                    }
+                    else
+                    {
+                        previousFo = max;
+                    }
+                    doppler.SetFo(previousFo);
+                    slider.value = previousFo;
+                }
+                else
+                {
+                    previousFo = v;
+                }
             }
             else if (tmp.name == "vs")
             {
                 doppler.SetVs(v);
+                if (!doppler.Check())
+                {
+                    min = doppler.getMinValueVs();
+                    max = doppler.getMaxValueVs();
+                    if (Mathf.Abs(v - max) > Mathf.Abs(v - min))
+                    {
+                        previousVs = min;
+                    }
+                    else
+                    {
+                        previousVs = max;
+                    }
+                    doppler.SetVs(previousVs);
+                    slider.value = previousVs;
+                }
+                else
+                {
+                    previousVs = v;
+                }
             }
         });
     }
